30d25327b8da18603ac3df7bbba445bdda44c5fd
Abraham Gellis <aagellis@gmail.com>
2013-09-28 22:37:09 -0400

    Finished assignment

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish : MonoBehaviour {
+	
+	public Vector3 destination;
+	public float moveSpeed;
+	public float attentionSpan;
+	
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("SetNewDestination", 0f, attentionSpan);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.LookRotation(rigidbody.velocity);
+		//transform.LookAt(destination);
+	}
+	
+	void FixedUpdate() {
+		Vector3 direction = Vector3.Normalize(destination - transform.position);
+		rigidbody.AddForce(moveSpeed * direction, ForceMode.VelocityChange);
+	}
+	
+	void SetNewDestination() {
+		destination = Random.insideUnitSphere * 30f;
+	}
+}
---
+++ Assets/Scripts/FishGod.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FishGod : MonoBehaviour {
+	
+	public Fish fishBlueprint;
+	public Fish mantaBlueprint;
+	public float fishToMantaRatio;
+	
+	public int count;
+	
+	List<Fish> mortals;
+	
+
+	// Use this for initialization
+	void Start () {
+		mortals = new List<Fish>();
+		mortals.Add(fishBlueprint);
+		mortals.Add(mantaBlueprint);
+		
+		int total = 0;
+		while (total < count) {
+			++total;
+			
+			Vector3 fishPosition = Random.insideUnitSphere * 10f;
+			
+			if (Random.Range(0f, 1f) > fishToMantaRatio)
+				mortals.Add(Instantiate(mantaBlueprint, fishPosition, Quaternion.identity) as Fish);
+			else
+				mortals.Add(Instantiate(fishBlueprint, fishPosition, Quaternion.identity) as Fish);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			foreach (Fish f in mortals)
+				f.destination = Vector3.zero;
+		}
+	}
+}
